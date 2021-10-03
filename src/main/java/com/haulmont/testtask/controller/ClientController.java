7ffa1efbe4497b2ce0d1d78a8d2dcab65301943b
hetty4332 <hetty4332@gmail.com>
2021-10-03 15:28:47 +0400

    Начали делать вьюшки. Надо разобраться с контроллером для добавления и рекдактирования клиентов

--- src/main/java/com/haulmont/testtask/controller/ClientController.java
+++ src/main/java/com/haulmont/testtask/controller/ClientController.java
@@ -1,18 +1,18 @@
 package com.haulmont.testtask.controller;
 
-import com.haulmont.testtask.model.Bank;
 import com.haulmont.testtask.model.Client;
-import com.haulmont.testtask.repository.BankRepository;
 import com.haulmont.testtask.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ClientController {
@@ -29,7 +29,7 @@ public class ClientController {
         return "clients";
     }
 
-    @PostMapping("/clients")
+    @PostMapping("/edit")
     public String addClient(@ModelAttribute("client") Client client) {
         clientRepository.save(client);
         return "clients";
@@ -38,6 +38,13 @@ public class ClientController {
     @PostMapping("/clients")
     public String deleteClient(@ModelAttribute("client") Client client) {
         clientRepository.delete(client);
+        return "clients";
+    }
+    @GetMapping("/edit/{id}")
+    public String getClient(@PathVariable Long id) {
+
+        Client client= clientRepository.findById(id).get();//TODO Сделать тут ElseThrow и выкидыватьк какую-нибудь ошибку, если объект null
+
         return "clients";
     }
 
